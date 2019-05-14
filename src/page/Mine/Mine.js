b00c3ed8411da438f1b21f34c0f61fbe559fc672
sshch3612 <1758974924@qq.com>
2019-05-14 10:37:23 +0800

    引入ListItem组件

--- src/page/Mine/Mine.js
+++ src/page/Mine/Mine.js
@@ -1,38 +1,34 @@
 import React from "react";
-import { 
-  View,
-  Text,
-  Share
-} from "react-native";
+import { Share } from "react-native";
+import { ListItem, Left, Right, Body,View, Text } from "native-base";
 import { createStackNavigator, createAppContainer } from "react-navigation";
-import Avatar from '../../Components/Avatar'
-import ListItem from '../../Components/ListItem';
-import FontAwesome from 'react-native-vector-icons/FontAwesome'
-
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import Avatar from "../../Components/Avatar";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import {autoHeight,autoWidth,autoFont} from '../../util/flex';
 
 export default class Mine extends React.Component {
-  static navigationOptions  = {
-    headerTitle:'我的',
+  static navigationOptions = {
+    headerTitle: "我的",
     headerBackTitle: null,
-    headerTitleStyle:{color:'red',textAlign:'center',flex:1},
-    headerStyle:{elevation:0},//elevation 可去除header 的阴影线
-  }
+    headerTitleStyle: { textAlign: "center", flex: 1 },
+    headerStyle: { elevation: 0 } //elevation 可去除header 的阴影线
+  };
   _basket = () => {
-    this.props.navigation.navigate('Basket')
-  }
+    this.props.navigation.navigate("Basket");
+  };
 
   _history = () => {
-    this.props.navigation.navigate('History')
-  }
+    this.props.navigation.navigate("History");
+  };
 
-  async onShare(){
+  async onShare() {
     try {
       const result = await Share.share({
         message:
-          'React Native | A framework for building native apps using React',
-      })
-      console.log(result,333);
+          "React Native | A framework for building native apps using React"
+      });
+      console.log(result, 333);
       if (result.action === Share.sharedAction) {
         if (result.activityType) {
           // shared with activity type of result.activityType
@@ -49,27 +45,51 @@ export default class Mine extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1}}>
-          <View style={style.avatar}>
-            <Avatar  size={100} round={true}/>
-            <Text style={style.login}>登陆 / 注册账号</Text>
-            <View style={style.descibe}> 
-              <Text style={{width:250,fontSize:17,textAlign:'center'}}>
-                解锁收藏、云端同步功能、收藏内容不丢失
-              </Text>
-            </View>
+      <View style={{ flex: 1 }}>
+        <View style={style.avatar}>
+          <Avatar size={100} round={true} />
+          <Text style={style.login}>登陆 / 注册账号</Text>
+          <View style={style.descibe}>
+            <Text style={{ fontSize: autoFont(10), textAlign: "center" }}>
+              解锁收藏、云端同步功能、收藏内容不丢失
+            </Text>
           </View>
-          <View>
-            <ListItem onPress={this._basket}
-               renderLeftIcon = {<FontAwesome style={{marginRight:5}} name='shopping-basket' size={22} color={'grey'}/>}
-               title='菜篮子' />
-               <ListItem  onPress={this._history}
-               renderLeftIcon = {<FontAwesome style={{marginRight:5}} name='history' size={22} color={'grey'}/>}
-               title='历史记录' />
-               <ListItem  onPress={this.onShare}
-               renderLeftIcon = {<AntDesign style={{marginRight:5}} name='like2' size={22} color={'grey'}/>}
-               title='用着不错,鼓励一下吧' />
-          </View>
+        </View>
+        <View>
+          <ListItem icon onPress={this._basket} >
+            <Left>
+              <FontAwesome name="shopping-basket" size={autoFont(16)} color={"grey"} />
+            </Left>
+            <Body style={{ borderBottomColor: "red", borderBottomWidth: 0 }}>
+              <Text>菜篮子</Text>
+            </Body>
+            <Right style={{ borderBottomColor: "red", borderBottomWidth: 0 }}>
+              <FontAwesome name="angle-right" size={autoFont(16)} color={"grey"} />
+            </Right>
+          </ListItem>
+          <ListItem icon onPress={this._history}>
+            <Left>
+              <FontAwesome name="history" size={autoFont(16)} color={"grey"} />
+            </Left>
+            <Body style={{ borderBottomColor: "red", borderBottomWidth: 0 }}>
+              <Text>历史记录</Text>
+            </Body>
+            <Right style={{ borderBottomColor: "red", borderBottomWidth: 0 }}>
+              <FontAwesome name="angle-right" size={autoFont(16)} color={"grey"} />
+            </Right>
+          </ListItem>
+          <ListItem icon onPress={this.onShare}>
+            <Left>
+              <AntDesign name="like2" size={autoFont(16)} color={"grey"} />
+            </Left>
+            <Body style={{ borderBottomColor: "red", borderBottomWidth: 0 }}>
+              <Text>用着不错，鼓励一下呗！</Text>
+            </Body>
+            <Right style={{ borderBottomColor: "red", borderBottomWidth: 0 }}>
+              <FontAwesome name="angle-right" size={autoFont(16)} color={"grey"} />
+            </Right>
+          </ListItem>
+        </View>
       </View>
     );
   }
@@ -77,12 +97,16 @@ export default class Mine extends React.Component {
 
 const style = {
   avatar: {
-    display:'flex',
-    alignItems:'center',
+    display: "flex",
+    alignItems: "center",
+    marginBottom: 10
   },
   login: {
-    fontSize:22,
+    fontSize:autoFont(17),
+    fontWeight:'normal',
+    marginBottom:5,
   },
   descibe: {
+    width:'50%',
   }
-}
+};
